2bc37d6b0621882deb2829dcbe22ecd5131b1ab5
Hassan Karaouni <hkII@DN0a22b626.SUNet>
2016-02-14 01:18:54 -0800

    Add rotation for hearts to stare at player

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -8,17 +8,18 @@ public class Heart : MonoBehaviour {
 
     public GameObject destructionPrefab;
 
+	private GameObject player;
     private float sentiment;
 
     // Use this for initialization
     void Start() {
-
+		player = GameObject.FindGameObjectWithTag ("Player");
     }
 
     // Update is called once per frame
     void Update() {
 
-    }
+	}
 
     public void SetSMSData(SMSManager.SMSData data) {
         GetComponentInChildren<Text> ().text = data.body + "\n" + "From: " + data.city;
---
+++ Assets/StareAtPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StareAtPlayer : MonoBehaviour {
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion rot = Quaternion.LookRotation (player.transform.position - transform.position);
+
+		transform.localEulerAngles = new Vector3(
+			0.0f,
+			rot.eulerAngles.y,
+			0.0f
+		);
+	}
+}
